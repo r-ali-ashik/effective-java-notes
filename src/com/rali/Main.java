72f2be43731956a82768fc1b88a3757dad1c441e
aliashik <ramjan.ali.ashik@gmail.com>
2020-09-30 22:02:50 +0600

    adding item-2

--- src/com/rali/Main.java
+++ src/com/rali/Main.java
@@ -1,7 +1,15 @@
 package com.rali;
 
+import com.rali.item.NutritionFacts;
+
 public class Main {
     public static void main(String[] args) {
+
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100)
+                .sodium(35)
+                .carbohydrate(27)
+                .build();
 
     }
 }
